a2fa7ca8adc8bfbba6bec96a1b67d884731284f9
Didik Hariyanto <com.didikhariyanto@gmail.com>
2017-12-23 14:18:39 +0700

    #1 first commit

---
+++ src/main/java/com/dihardmg/employee/crudemployee/CrudEmployeeApplication.java
@@ -0,0 +1,12 @@
+package com.dihardmg.employee.crudemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmployeeApplication.class, args);
+	}
+}
