2670b79f30f5778b9f9964a1a0652dcf12139913
Didik Hariyanto <com.didikhariyanto@gmail.com>
2018-01-03 19:53:15 +0700

    Update V2__insert_data.sql
    fix sample data

--- src/main/resources/db/migration/V2__insert_data.sql
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -7,13 +7,13 @@ INSERT INTO employee VALUES
   ('aa6','jiraya','naruto5@gmail.com'),
   ('aa7','sino','naruto6@gmail.com');
 
-INSERT INTO `s_roles` VALUES
+INSERT INTO s_roles VALUES
 ('adm','ROLE_ADMIN'),('stf','ROLE_STAFF');
 
-INSERT INTO `s_users` VALUES
+INSERT INTO s_users VALUES
 ('u001','didik','$2a$10$T0lWqk.pk2guoCxhCqPS9.HHxX6TSIL2559Gk7qNIF0hODKtTXda.',true),('u002','adi','$2a$10$T0lWqk.pk2guoCxhCqPS9.HHxX6TSIL2559Gk7qNIF0hODKtTXda.',true);
 
-INSERT INTO `s_user_role` VALUES
+INSERT INTO s_user_role VALUES
 ('u001','adm'),('u001','stf'),('u002','stf');
 
 
